c5cc9ceb0899e91878d4fec7f28868cf9b215682
EZEN <EZEN@DESKTOP-Q6ML8EM>
2020-01-17 17:33:59 +0900

    공고 즐겨찾기 완료 by 김승수

--- jobsearch/src/main/java/com/ezen/jobsearch/scrap/model/ScrapService.java
+++ jobsearch/src/main/java/com/ezen/jobsearch/scrap/model/ScrapService.java
@@ -3,4 +3,5 @@ package com.ezen.jobsearch.scrap.model;
 public interface ScrapService {
 	int insertScrap(ScrapVO scrapVo);
 	int deleteScrap(ScrapVO scrapVo);
+	int selectScrapYN(ScrapVO scrapVo);
 }
