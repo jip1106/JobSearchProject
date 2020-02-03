3639d68be08db74b05da073ce47dbc7abc0de682
EZEN <EZEN@DESKTOP-Q6ML8EM>
2020-02-03 15:04:44 +0900

    즐겨찾기 오류 수정 by 김승수

--- jobsearch/src/main/java/com/ezen/jobsearch/controller/LoginInterceptor.java
+++ jobsearch/src/main/java/com/ezen/jobsearch/controller/LoginInterceptor.java
@@ -54,12 +54,11 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
 			out.print("alert('관리자만 접근 가능합니다.');");
 			out.print("location.href='" + request.getContextPath() + loginPath +"';");
 			out.print("</script>");
-		
+
 			return false;
 		
 		//로그인은 했지만 일반회원이 아니면서 요청이 개인회원용일때	
 		}else if(!memberVo.getRegType().equals("1") && (request.getRequestURI().indexOf("mypage") > 0 && request.getRequestURI().indexOf("com") == 0 
-				|| request.getRequestURI().indexOf("Scrap") > 0
 				|| request.getRequestURI().indexOf("resume") > 0
 				|| request.getRequestURI().indexOf("apply") > 0) ) {
 			
@@ -69,7 +68,7 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
 			out.print("alert('일반회원으로  로그인하세요.');");
 			out.print("location.href='" + request.getContextPath() + loginPath +"';");
 			out.print("</script>");
-									
+
 			return false;
 		
 		//로그인은 했지만 기업회원이 아니면서 요청이 company일때
